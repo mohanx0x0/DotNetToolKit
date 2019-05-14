c8ad8d24ca4033b78a16a8a42e5f0f20f4c97b06
prasant_sp3214 <prasant_sp3214@hotmail.com>
2019-05-14 10:45:13 +0530

    Project Creation Initial Commit

---
+++ DotNetToolKit/ExportTools/GridViewExports.cs
@@ -0,0 +1,75 @@
+﻿using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DotNetToolKit.ExportTools
+{
+    public static class GridViewExports
+    {
+        static object LockMulti = new object();
+        public static DataTable GridViewToDataTable(GridView sourceGrid)
+        {
+            lock (LockMulti)
+            {
+                DataTable destinationTable = new DataTable();
+                for (int i = 0; i < sourceGrid.Columns.Count; i++)
+                    destinationTable.Columns.Add(sourceGrid.Columns[i].ToString());
+
+                foreach (GridViewRow row in sourceGrid.Rows)
+                {
+                    DataRow dr = destinationTable.NewRow();
+                    for (int j = 0; j < sourceGrid.Columns.Count; j++)
+                        if (!row.Cells[j].Text.Equals("&nbsp;"))
+                            dr[sourceGrid.Columns[j].ToString()] = row.Cells[j].Text;
+
+                    destinationTable.Rows.Add(dr);
+                }
+
+                return destinationTable;
+            }
+        }
+
+        public static void GridViewToExcel(GridView sourceGrid, string fileName)
+        {
+            lock (LockMulti)
+            {
+                HttpContext.Current.Response.Clear();
+                HttpContext.Current.Response.Buffer = true;
+                HttpContext.Current.Response.AddHeader("content-disposition", "attachment;filename=" + fileName + ".xls");
+                HttpContext.Current.Response.Charset = "";
+                HttpContext.Current.Response.ContentType = "application/vnd.ms-excel";
+                using (StringWriter sw = new StringWriter())
+                {
+                    HtmlTextWriter hw = new HtmlTextWriter(sw);
+                    sourceGrid.AllowPaging = false;
+                    sourceGrid.HeaderRow.BackColor = Color.White;
+                    foreach (TableCell cell in sourceGrid.HeaderRow.Cells)
+                    {
+                        cell.BackColor = sourceGrid.HeaderStyle.BackColor;
+                    }
+                    foreach (GridViewRow row in sourceGrid.Rows)
+                    {
+                        row.BackColor = Color.White;
+                        foreach (TableCell cell in row.Cells)
+                        {
+                            if (row.RowIndex % 2 == 0)
+                                cell.BackColor = sourceGrid.AlternatingRowStyle.BackColor;
+                            else
+                                cell.BackColor = sourceGrid.RowStyle.BackColor;
+                            cell.CssClass = "textmode";
+                        }
+                    }
+                    sourceGrid.RenderControl(hw);
+                    HttpContext.Current.Response.Output.Write(sw.ToString());
+                    HttpContext.Current.Response.Flush();
+                    HttpContext.Current.Response.End();
+                }
+            }
+        }
+    }
+
+
+}
---
+++ DotNetToolKit/Extensions/Extensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+using System.Globalization;
+
+namespace DotNetToolKit.Extensions
+{
+    public static class Extensions
+    {
+        public static string IndianString(this decimal number)
+        {
+            return number.ToString("#,#.##", new CultureInfo(0x0439));
+        }
+        public static string IndianString(this int number)
+        {
+            return number.ToString("#,#", new CultureInfo(0x0439));
+        }
+        public static string IndianString(this decimal? number)
+        {
+            return number.ToString();
+        }
+        public static string IndianString(this int? number)
+        {
+            return number.ToString();
+        }
+
+        public static void RemoveColumn(this DataTable table, string columnName)
+        {
+            DataColumnCollection columns = table.Columns;
+            if (columns.Contains(columnName))
+                table.Columns.Remove(columnName);
+        }
+    }
+}
